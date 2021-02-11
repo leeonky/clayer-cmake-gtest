db16e193928d23f0889517e4bca69bdfe998ff98
leeonky <leeonky@gmail.com>
2021-02-11 07:11:02 +0000

    Update cmake

--- cmake/shared-lib.cmake
+++ cmake/shared-lib.cmake
@@ -1,18 +1,20 @@
-
-function(shared_lib)
-	project(${CMAKE_PROJECT_NAME})
+macro(shared_lib)
 	aux_source_directory(. PATH_SOURCE)
 
-	add_library (${CMAKE_PROJECT_NAME} SHARED ${PATH_SOURCE})
+	add_library (${PROJECT_NAME} SHARED ${PATH_SOURCE})
+endmacro()
 
-	# For test
+macro(define_test_runner source)
+    set(runner_source ${source})
+endmacro()
+
+macro(add_test_runner)
 	find_package(GTest REQUIRED)
 	include_directories(${GTEST_INCLUDE_DIRS})
 
-	aux_source_directory(./test PATH_TEST)
-
-	add_executable (${CMAKE_PROJECT_NAME}.tests ${PATH_TEST})
-	target_link_libraries(${CMAKE_PROJECT_NAME}.tests ${GTEST_LIBRARIES} pthread)
-	add_test(${CMAKE_PROJECT_NAME} ${CMAKE_PROJECT_NAME}.tests)
-endfunction()
-
+	string(REPLACE test/ "" main_target ${runner_source})
+	set(main_target ${main_target}.tests)
+	add_executable (${main_target} ${runner_source})
+	target_link_libraries(${main_target} ${GTEST_LIBRARIES} pthread)
+	add_test(${main_target} ${main_target})
+endmacro()
